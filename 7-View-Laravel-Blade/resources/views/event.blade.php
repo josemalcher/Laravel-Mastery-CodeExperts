6a75aafe64cca78944a8e65557e7beb2a2a5a11f
José Malcher Jr <contato@josemalcher.net>
2021-07-10 18:15:18 -0300

    7. View: Laravel Blade - 70 - Compondo View Single Evento

--- 7-View-Laravel-Blade/resources/views/event.blade.php
+++ 7-View-Laravel-Blade/resources/views/event.blade.php
@@ -8,7 +8,31 @@
 
     <div class="row">
         <div class="col-12">
-            Evento: {{$event->title}}
+            <h2> Evento: {{$event->title}}</h2>
+            <p>Evento ocorrerá em {{$event->start_event->format('d/m/Y H:i')}}</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link active" id="about-tab" data-toggle="tab" href="#about" role="tab"
+                       aria-controls="home" aria-selected="true">SOBRE</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="photos-tab" data-toggle="tab" href="#photos" role="tab"
+                       aria-controls="profile" aria-selected="false">Photos</a>
+                </li>
+            </ul>
+            <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="about" role="tabpanel" aria-labelledby="about-tab">
+                    {{$event->body}}
+                </div>
+                <div class="tab-pane fade" id="photos" role="tabpanel" aria-labelledby="photos-tab">
+
+                </div>
+            </div>
         </div>
     </div>
 
