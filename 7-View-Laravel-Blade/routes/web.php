2a27ab9e0212e5b29ed9b0a4a31c50889b262b3c
José Malcher Jr <contato@josemalcher.net>
2021-07-12 15:29:07 -0300

    9. Laravel Router - 87 - Organizando Rotas com Prefixo e Grupo

--- 7-View-Laravel-Blade/routes/web.php
+++ 7-View-Laravel-Blade/routes/web.php
@@ -21,73 +21,22 @@ Route::get('hello', [\App\Http\Controllers\HelloWorldController::class, 'hellowo
 Route::get('/hello-name/{name?}', [\App\Http\Controllers\HelloWorldController::class, 'hello']);
 
 Route::get('/queries/{event?}', function ($event = null) {
-
-    //$events = \App\Models\Event::all();
-    //$events = \App\Models\Event::all(['title', 'description']);
-
-    //$event =  \App\Models\Event::where('id', 1)->get();
-
-//  $event =  \App\Models\Event::where('id', 1)->first();
-    //$events = \App\Models\Event::find($event);
-
-    // insert into events(title, description, body, start_event) values(?,?,?,?)
-    //Active Record
-    /*
-    $event = new \App\Models\Event();
-    $event->title = 'Evento TESTE AR 2';
-    $event->description = 'Evento gravado via AR 2';
-    $event->body = 'conteudo...';
-    $event->start_event = date('Y-m-d H:i:s');
-    $event->slug = \Illuminate\Support\Str::slug($event->title);
-    */
-
-    //update event set title = ? , description = ? (...) where id = ?
-    /*  $event = \App\Models\Event::find(32);
-        $event->title = 'EVENTO 2 Atualizado';
-        $event->slug  = \Illuminate\Support\Str::slug($event->title);
-    */
-
-    //return $event->save();
-    //return $event;
-
-    // Update em massa
-    /*
-        $eventData = [
-            //    'title' => 'Titulo add 3',
-            'description' => 'Descrição 3333 UPDATE MASS',
-            'body' => 'Corpo 3 UPDATE MASS',
-            //    'start_event' => date('Y-m-d H:i:s'),
-            //    'slug' => 'titulo-adddddd-3-with-array-3'
-        ];
-
-        $event = \App\Models\Event::find(33);
-        $event->update($eventData);
-    */
-
-    /*  $event = \App\Models\Event::findOrFail(1);
-        return $event->delete();
-    */
-    //Delete Models via ids  [ 2,3,4,5]
-    //return \App\Models\Event::destroy([2, 3, 4, 5]);
-
-    //Select * from events order by id ASC limt 3
     return \App\Models\Event::orderBy('id', 'ASC')->limit(3)->get();
 });
-
-//Rotas CRUD BASE para evenos...
-Route::get('/admin/events/index', [\App\Http\Controllers\Admin\EventController::class, 'index']);
-Route::get('/admin/events/create', [\App\Http\Controllers\Admin\EventController::class, 'create']);
-
-Route::post('/admin/events/store', [\App\Http\Controllers\Admin\EventController::class, 'store']);
-
-Route::get('/admin/events/{event}/edit', [\App\Http\Controllers\Admin\EventController::class, 'edit']);
-
-Route::post('/admin/events/update/{event}', [\App\Http\Controllers\Admin\EventController::class, 'update']);
-Route::get('/admin/events/destroy/{event}', [\App\Http\Controllers\Admin\EventController::class, 'destroy']);
-
 Route::get('/eventos/{slug}', [\App\Http\Controllers\HomeController::class, 'show']);
 
+//Rotas CRUD BASE para evetos...
 
+Route::prefix('/admin')->group(function () {
+    Route::prefix('events')->group(function () {
+        Route::get ('/index',          [\App\Http\Controllers\Admin\EventController::class, 'index']);
+        Route::get ('/create',         [\App\Http\Controllers\Admin\EventController::class, 'create']);
+        Route::post('/store',          [\App\Http\Controllers\Admin\EventController::class, 'store']);
+        Route::get ('/{event}/edit',   [\App\Http\Controllers\Admin\EventController::class, 'edit']);
+        Route::post('/update/{event}', [\App\Http\Controllers\Admin\EventController::class, 'update']);
+        Route::get ('/destroy/{event}',[\App\Http\Controllers\Admin\EventController::class, 'destroy']);
+    });
+});
 
 // GET | POST | PUT | DELETE | OPTIONS | HEAD
 
