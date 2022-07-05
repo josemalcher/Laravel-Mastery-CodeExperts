1390fa4fc389e2bfa139796050f81ddb17aa3ef7
Jose Malcher Jr <contato@josemalcher.net>
2022-07-05 14:51:49 -0300

    70 Compondo View Single Evento

--- 03-PrimeirosPassosVisaoGeral/projMeusEventos/resources/views/event.blade.php
+++ 03-PrimeirosPassosVisaoGeral/projMeusEventos/resources/views/event.blade.php
@@ -8,7 +8,26 @@ Evento - {{$event->title}}
 
     <div class="row">
         <div class="col-12">
-            Evento: {{$event->title}}
+            <h2>Evento: {{$event->title}}</h2>
+            <p>Evento Ocorrerá: {{$event->start_event->format('d/m/Y H:i:s')}}</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#sobre" role="tab" aria-controls="home" aria-selected="true">Sobre</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#fotos" role="tab" aria-controls="profile" aria-selected="false">Fotos</a>
+                </li>
+            </ul>
+            <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="sobre" role="tabpanel" aria-labelledby="home-tab">
+                    {{$event->body}}
+                </div>
+                <div class="tab-pane fade" id="fotos" role="tabpanel" aria-labelledby="profile-tab">...</div>
+            </div>
         </div>
     </div>
 
