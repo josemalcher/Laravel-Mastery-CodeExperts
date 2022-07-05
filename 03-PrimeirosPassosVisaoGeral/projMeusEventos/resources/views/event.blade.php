5f8fc862fd66bd8db4cc64042dfeee8549909765
Jose Malcher Jr <contato@josemalcher.net>
2022-07-05 15:11:19 -0300

    71 Exibindo Fotos Evento se Existirem

--- 03-PrimeirosPassosVisaoGeral/projMeusEventos/resources/views/event.blade.php
+++ 03-PrimeirosPassosVisaoGeral/projMeusEventos/resources/views/event.blade.php
@@ -18,15 +18,28 @@ Evento - {{$event->title}}
                 <li class="nav-item" role="presentation">
                     <a class="nav-link active" id="home-tab" data-toggle="tab" href="#sobre" role="tab" aria-controls="home" aria-selected="true">Sobre</a>
                 </li>
-                <li class="nav-item" role="presentation">
-                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#fotos" role="tab" aria-controls="profile" aria-selected="false">Fotos</a>
-                </li>
+                @if($event->photos->count())
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#fotos" role="tab"
+                           aria-controls="profile" aria-selected="false">Fotos</a>
+                    </li>
+                @endif
             </ul>
             <div class="tab-content" id="myTabContent">
-                <div class="tab-pane fade show active" id="sobre" role="tabpanel" aria-labelledby="home-tab">
+                <div class="tab-pane fade show active pt-5" id="sobre" role="tabpanel" aria-labelledby="home-tab">
                     {{$event->body}}
                 </div>
-                <div class="tab-pane fade" id="fotos" role="tabpanel" aria-labelledby="profile-tab">...</div>
+                @if($event->photos->count())
+                    <div class="tab-pane fade pt-5" id="fotos" role="tabpanel" aria-labelledby="profile-tab">
+                        <div class="row">
+                            @foreach($event->photos as $photo)
+                                <div class="col-3">
+                                    <img src="{{$photo->photo}}" alt="Foto do Evento {{$event->title}}" class="img-fluid">
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                @endif
             </div>
         </div>
     </div>
