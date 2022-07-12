e63f8a9997f216e8438d87fd64a9e9f9b97b569c
Jose Malcher Jr <contato@josemalcher.net>
2022-07-11 22:29:33 -0300

    78 Tela de Criação de Evento

--- 03-PrimeirosPassosVisaoGeral/projMeusEventos/app/Http/Controllers/Admin/EventController.php
+++ 03-PrimeirosPassosVisaoGeral/projMeusEventos/app/Http/Controllers/Admin/EventController.php
@@ -16,6 +16,11 @@ class EventController extends Controller
         return view('admin.events.index', compact('events'));
     }
 
+    public function create()
+    {
+        return view('admin.events.create');
+    }
+
     public function store()
     {
         $event = [
---
+++ 03-PrimeirosPassosVisaoGeral/projMeusEventos/resources/views/admin/events/create.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('title')
+    Criar Evento -
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-12 my-5">
+            <h2>Criar Evento</h2>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            <form action="/admin/events/store" method="post">
+                <div class="form-group">
+                    <label>Título Evento</label>
+                    <input type="text" class="form-control" name="title">
+                </div>
+                <div class="form-group">
+                    <label>Descrição Rápida Evento</label>
+                    <input type="text" class="form-control" name="description">
+                </div>
+                <div class="form-group">
+                    <label>Fale Mais sobre o Evento</label>
+                    <textarea name="body" id="" cols="30" rows="10" class="form-control"></textarea>
+                </div>
+                <div class="form-group">
+                    <label>Fale Mais sobre o Evento</label>
+                    <input type="text" class="form-control" name="start_event">
+                </div>
+                <button type="submit" class="btn btn-lg btn-success">Criar Evento</button>
+            </form>
+        </div>
+    </div>
+@endsection
