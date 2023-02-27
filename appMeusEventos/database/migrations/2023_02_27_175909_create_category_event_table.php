ee8f85bd84995bc17fe6046d0cd0eb3ced870c96
Jose Malcher Jr <contato@josemalcher.net>
2023-02-27 15:00:42 -0300

    50 Migração N:N

---
+++ appMeusEventos/database/migrations/2023_02_27_175909_create_category_event_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryEventTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_event', function (Blueprint $table) {
+//            $table->id();
+//            $table->timestamps();
+            $table->foreignId('event_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_event');
+    }
+}
