f32be7138b68b52619b4331142858e3ee74c400b
Jose Malcher Jr <contato@josemalcher.net>
2023-03-02 12:04:49 -0300

    71 Exibindo Fotos Evento se Existirem

--- appMeusEventos/resources/views/event.blade.php
+++ appMeusEventos/resources/views/event.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <div class="row">
         <div class="col-12">
-           <h2> Evento: {{$event->title}}</h2>
+            <h2> Evento: {{$event->title}}</h2>
         </div>
     </div>
     <div class="row">
@@ -17,20 +17,34 @@
                 <li class="nav-item" role="presentation">
                     <button class="nav-link active" id="home-tab"
                             data-toggle="tab" data-target="#home" type="button" role="tab"
-                            aria-controls="home" aria-selected="true">Sobre</button>
+                            aria-controls="home" aria-selected="true">Sobre
+                    </button>
                 </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="profile-tab"
-                            data-toggle="tab" data-target="#profile" type="button" role="tab"
-                            aria-controls="profile" aria-selected="false">Fotos</button>
-                </li>
-
+                @if($event->photos->count())
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="profile-tab"
+                                data-toggle="tab" data-target="#profile" type="button" role="tab"
+                                aria-controls="profile" aria-selected="false">Fotos
+                        </button>
+                    </li>
+                @endif
             </ul>
             <div class="tab-content" id="myTabContent">
-                <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                <div class="tab-pane fade pt-5 show active" id="home" role="tabpanel" aria-labelledby="home-tab">
                     {{ $event->body }}
                 </div>
-                <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+                @if($event->photos->count())
+                    <div class="tab-pane fade pt-5" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                        <div class="row">
+                            @foreach($event->photos as $photo)
+                                <div class="col-3">
+                                    <img src="{{$photo->photo}}" alt="Foto do Evento {{$event->title}}"
+                                         class="img-fluid">
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                @endif
             </div>
         </div>
     </div>
