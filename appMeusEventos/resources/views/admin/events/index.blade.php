80fbda0e0244fbafb7d4dccb8c5618ccae5b9a31
Jose Malcher Jr <contato@josemalcher.net>
2023-03-03 16:28:07 -0300

    76 Listagem de Eventos

---
+++ appMeusEventos/resources/views/admin/events/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.site')
+
+@section('title')
+    Meus Eventos
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <table class="table table-rounded table-striped">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Evento</th>
+                    <th>Criado em</th>
+                </tr>
+                </thead>
+                <tbody>
+                @forelse($events as $event)
+                    <tr>
+                        <td>{{ $event->id }}</td>
+                        <td>{{ $event->title }}</td>
+                        <td>{{ $event->created_at->format('d/m/Y H:i') }}</td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="3">Nenhum evento Encontrado</td>
+                    </tr>
+
+                @endforelse
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+@endsection
---
+++ appMeusEventos/resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="pt_BR'">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+          integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <title>@yield('title') - Administrar Eventos</title>
+</head>
+<body>
+
+<div class="container">
+    @yield('content') <!--todas as views quie extenderem desta colocaram seu conteudo nesta área-->
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
+        crossorigin="anonymous"></script>
+</body>
+</html>
