dc109b8bca8dc2d5c3d94bf23f3ddee2014c63f0
Jose Malcher Jr <contato@josemalcher.net>
2023-03-07 16:05:39 -0300

    95 Exibindo Todas as Validações na View

--- appMeusEventos/resources/views/admin/events/create.blade.php
+++ appMeusEventos/resources/views/admin/events/create.blade.php
@@ -1,43 +1,54 @@
 @extends('layouts.app')
 
 @section('title')
-    Criar Evento
+Criar Evento
 @endsection
 
 @section('content')
-    <div class="row">
-        <div class="col-12 my-5">
-            <h2>Criar Evento</h2>
-        </div>
+<div class="row">
+    <div class="col-12 my-5">
+        <h2>Criar Evento</h2>
     </div>
-    <div class="row">
-        <div class="col-12">
-            <form action="{{route('admin.events.store')}}" method="post">
+</div>
 
-                @csrf
+@if($errors->any())
+<div class="alert alert-danger">
+    <ul>
+        @foreach($errors->all() as $erro)
+        <li>{{$erro}}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
 
-                <div class="form-group">
-                    <label>Título do Evento</label>
-                    <input type="text" class="form-control" name="title">
-                </div>
+<div class="row">
+    <div class="col-12">
+        <form action="{{route('admin.events.store')}}" method="post">
 
-                <div class="form-group">
-                    <label>Descrição</label>
-                    <input type="text" class="form-control" name="description">
-                </div>
+            @csrf
 
-                <div class="form-group">
-                    <label>Fale Mais sobre</label>
-                    <textarea name="body" id="" cols="30" rows="10" class="form-control"></textarea>
-                </div>
+            <div class="form-group">
+                <label>Título do Evento</label>
+                <input type="text" class="form-control" name="title">
+            </div>
 
-                <div class="form-group">
-                    <label>Quando Ocorrerá</label>
-                    <input type="text" class="form-control" name="start_event">
-                </div>
-                <button type="submit" class="btn btn-lg btn-success">Criar Evento</button>
-            </form>
-        </div>
+            <div class="form-group">
+                <label>Descrição</label>
+                <input type="text" class="form-control" name="description">
+            </div>
+
+            <div class="form-group">
+                <label>Fale Mais sobre</label>
+                <textarea name="body" id="" cols="30" rows="10" class="form-control"></textarea>
+            </div>
+
+            <div class="form-group">
+                <label>Quando Ocorrerá</label>
+                <input type="text" class="form-control" name="start_event">
+            </div>
+            <button type="submit" class="btn btn-lg btn-success">Criar Evento</button>
+        </form>
     </div>
+</div>
 
 @endsection
