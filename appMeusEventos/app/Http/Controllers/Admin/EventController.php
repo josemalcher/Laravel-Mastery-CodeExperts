33ac44ad8dd174fef374ae0d51f9ad90e377a37b
Jose Malcher Jr <contato@josemalcher.net>
2023-03-08 09:46:20 -0300

    100 Validação na Edição do Evento

--- appMeusEventos/app/Http/Controllers/Admin/EventController.php
+++ appMeusEventos/app/Http/Controllers/Admin/EventController.php
@@ -54,7 +54,7 @@ class EventController extends Controller
         return view('admin.events.edit', compact('event'));
     }
 
-    public function update($event, Request $request)
+    public function update($event, EventRequest $request)
     {
         $event = Event::findorFail($event);
         $event->update($request->all());
