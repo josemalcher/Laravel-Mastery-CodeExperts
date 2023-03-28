355eee304eae5794c1b58a096dcc965442798494
Jose Malcher Jr <contato@josemalcher.net>
2023-03-28 15:14:59 -0300

    209 Conclusões

--- videoFlix/resources/views/layouts/navigation.blade.php
+++ videoFlix/resources/views/layouts/navigation.blade.php
@@ -72,6 +72,11 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('content.index')" :active="request()->routeIs('content*')">
+                Conteúdo
+            </x-responsive-nav-link>
+
         </div>
 
         <!-- Responsive Settings Options -->
