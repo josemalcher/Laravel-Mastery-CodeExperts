09a59cffb4505480a05b57ddaaefc1c87027e445
José Malcher Jr. <contato@josemalcher.net>
2025-01-21 23:13:22 -0300

    [REV] 29 Falando Sobre Migrações

--- app2025/database/migrations/2025_01_22_011756_create_events_table.php
+++ app2025/database/migrations/2025_01_22_011756_create_events_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('description');
+            $table->text('body');
+            $table->dateTime('start');
+            $table->dateTime('end');
             $table->timestamps();
         });
     }
--- app2025/database/seeders/DatabaseSeeder.php
+++ app2025/database/seeders/DatabaseSeeder.php
@@ -13,11 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        //User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        //\App\Models\Event::factory(30)->create();
+
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
+        $this->call(UsersTableSeeder::class);
+        $this->call(EventsTableSeeder::class);
     }
 }
