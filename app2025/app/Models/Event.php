36826239dd0660a3dc68439677339dac00b518c0
José Malcher Jr. <contato@josemalcher.net>
2025-01-23 07:45:01 -0300

    [REV] 39 Removendo Dados

--- app2025/app/Models/Event.php
+++ app2025/app/Models/Event.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'body',
+        'slug',
+        'start',
+        'end'
+    ];
+
 }
--- app2025/routes/web.php
+++ app2025/routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+/*
 Route::get('/queries/{id}', function ($id) {
     // $events = \App\Models\Event::all(); // select * from events
     // $events = \App\Models\Event::all(['title', 'description']); // select title, description from events
@@ -15,4 +15,68 @@ Route::get('/queries/{id}', function ($id) {
     $events = \App\Models\Event::find($id); // select * from events WHERE id = 1
 
     return $events;
+});
+
+Route::get('/queries/{event?}', function ($event = null){
+
+    $event = new \App\Models\Event();
+    $event->title = 'Evento TESTE via Eloquent e AR';
+    $event->description = 'Evento teste';
+    $event->body = 'corpo do evento';
+    $event->start = date('Y-m-d H:i:s');
+    $event->end = date('Y-m-d H:i:s');
+    $event->slug = \Illuminate\Support\Str::slug($event->title);
+
+    return $event->save();
+
+});
+
+Route::get('/queries/{event?}', function ($event = null){
+
+    $event = \App\Models\Event::find(1);
+    $event->title = 'Evento ATUALIZADO';
+    $event->slug = \Illuminate\Support\Str::slug($event->title);
+
+    return $event->save();
+
+});
+
+Route::get('/queries/{event?}', function ($event = null){
+
+    // Atribuição Massa ou Mass Assingnment
+    $event = [
+        'title' => 'Evento Atribuição em Massa',
+        'description' => 'Descrição',
+        'body' => 'Conteudo do Evento',
+        'slug' => 'evento-atribuicao-em-massa',
+        'start' => date('Y-m-d H:i:s'),
+        'end' => date('Y-m-d H:i:s')
+    ];
+
+    return \App\Models\Event::create($event);
+
+});*/
+
+Route::get('/queries/{event?}', function ($event = null){
+/*
+    $eventDATA = [
+        // 'title' => 'Evento Atribuição em Massa',
+        'description' => 'Descrição ATUALIZADA',
+        // 'body' => 'Conteudo do Evento',
+        // 'slug' => 'evento-atribuicao-em-massa',
+        // 'start_event' => date('Y-m-d H:i:s')
+    ];
+
+    $event = \App\Models\Event::find(1);
+    $event->update($eventDATA);
+
+    return $event;*/
+
+
+    // $event = \App\Models\Event::findOrFail(2);
+    //return $event->delete(); // 1
+
+    return \App\Models\Event::destroy([8,7,6]); // 3
+
+
 });
