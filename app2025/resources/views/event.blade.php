5dfb4c63f3a00d95202cd77c28fa112f780a57a6
José Malcher Jr. <contato@josemalcher.net>
2025-03-17 21:46:19 -0300

    [REV] 70 Compondo View Single Evento

--- app2025/resources/views/event.blade.php
+++ app2025/resources/views/event.blade.php
@@ -8,7 +8,23 @@
 
     <div class="row">
         <div class="col-12">
-            <h1>Evento: {{$event->title}}</h1>
+            <h2>Evento: {{$event->title}}</h2>
+            <strong>Acontece em: {{$event->start->format('d/m/Y H:i:s')}}</strong>
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#sobre" role="tab" aria-controls="home" aria-selected="true">Sobre</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#fotos" role="tab" aria-controls="profile" aria-selected="false">Fotos</a>
+                </li>
+
+            </ul>
+            <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="sobre" role="tabpanel" aria-labelledby="home-tab">
+                    <p>{{$event->description}}</p>
+                </div>
+                <div class="tab-pane fade" id="fotos" role="tabpanel" aria-labelledby="profile-tab">..2.</div>
+            </div>
         </div>
     </div>
 
